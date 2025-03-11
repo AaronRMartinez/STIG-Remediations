a183646c2e6a6f7e330d75961825af96ae93dc9f
Aaron Martinez <mars0750@gmail.com>
2025-03-11 14:26:55 -0400

    Create WN10-CC-000068.ps1

---
+++ WN10-CC-000068.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Windows 10 is configured to enable Remote host to allow delegation of non-exportable credentials.
+    Author          : Aaron Martinez
+    LinkedIn        : linkedin.com/in/aaron-m-59725a332/
+    GitHub          : github.com/AaronRMartinez
+    Date Created    : 2025-03-11
+    Last Modified   : 2025-03-11
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000068
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN10-CC-000068.ps1
+#>
+
+# Define registry path and value
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation"
+$ValueName = "AllowProtectedCreds"
+$ValueData = 1  # Enforce protected credentials delegation
+
+# Ensure the registry path exists
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Set the registry value
+Set-ItemProperty -Path $RegPath -Name $ValueName -Value $ValueData -Type DWord
+
+Write-Output "Successfully set $ValueName to $ValueData at $RegPath"
+
+# Force policy update
+gpupdate /force
